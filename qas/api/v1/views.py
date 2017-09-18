3c967503bdbf98ef61e2fecbf39c00ed43053426
Ankur Gupta <ankur0493@gmail.com>
2017-09-18 17:30:38 +0530

    Add page number pagination to the question API

--- qas/api/v1/views.py
+++ qas/api/v1/views.py
@@ -1,18 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, pagination
 
 from qas.models import Question
 from .permissions import TentantAPIKeyPermission
 from .serializers import QuestionSerializer
 from .throttling import TenantRateThrottle
 
+class CustomPageNumberPagination(pagination.PageNumberPagination):
+    page_size = 10
+
+
 class QuestionListView(generics.ListAPIView):
     serializer_class = QuestionSerializer
     permission_classes = (TentantAPIKeyPermission, )
     throttle_classes = (TenantRateThrottle, )
     queryset = Question.objects.exclude(private=True)
+    pagination_class = CustomPageNumberPagination
 
     def get_queryset(self):
-        queryset = Question.objects.exclude(private=True)
+        queryset = Question.objects.exclude(private=True).order_by('id')
         query = self.request.query_params.get("q", "")
         if query:
             queryset = queryset.filter(title__icontains=query)
